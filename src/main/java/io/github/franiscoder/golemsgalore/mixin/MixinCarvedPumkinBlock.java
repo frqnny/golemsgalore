6b337ecaed730293d231e6e16acc19779cf05db7
ffrann <45723631+franiscoder@users.noreply.github.com>
2020-11-27 18:01:52 -0500

    Cleaned MixinCarvedPumkinBlock

--- src/main/java/io/github/franiscoder/golemsgalore/mixin/MixinCarvedPumkinBlock.java
+++ src/main/java/io/github/franiscoder/golemsgalore/mixin/MixinCarvedPumkinBlock.java
@@ -100,6 +100,42 @@ public class MixinCarvedPumkinBlock extends Block {
         super(settings);
     }
 
+    private static <T extends ModGolemEntity> void spawnGolem(World world, PlayerEntity player, Hand hand, BlockPattern.Result resultPattern, Optional<T> golem) {
+        if (!player.abilities.creativeMode) {
+            player.getStackInHand(hand).decrement(1);
+        }
+
+        int width = commonPattern.getWidth();
+        int height = commonPattern.getHeight();
+
+        for (int k = 0; k < width; ++k) {
+            for (int l = 0; l < height; ++l) {
+                CachedBlockPosition cachedBlockPosition3 = resultPattern.translate(k, l, 0);
+                world.setBlockState(cachedBlockPosition3.getBlockPos(), Blocks.AIR.getDefaultState(), 2);
+                world.syncWorldEvent(2001, cachedBlockPosition3.getBlockPos(), Block.getRawIdFromState(cachedBlockPosition3.getBlockState()));
+            }
+        }
+
+        if (golem.isPresent()) {
+            golem.get().setPlayerCreated(true);
+
+            BlockPos blockPos2 = resultPattern.translate(1, 2, 0).getBlockPos();
+            golem.get().refreshPositionAndAngles((double) blockPos2.getX() + 0.5D, (double) blockPos2.getY() + 0.05D, (double) blockPos2.getZ() + 0.5D, 0.0F, 0.0F);
+            world.spawnEntity(golem.get());
+
+            for (ServerPlayerEntity serverPlayer : world.getNonSpectatingEntities(ServerPlayerEntity.class, golem.get().getBoundingBox().expand(5.0D))) {
+                Criteria.SUMMONED_ENTITY.trigger(serverPlayer, golem.get());
+            }
+
+            for (int m = 0; m < width; ++m) {
+                for (int n = 0; n < height; ++n) {
+                    CachedBlockPosition cachedBlockPosition4 = resultPattern.translate(m, n, 0);
+                    world.updateNeighbors(cachedBlockPosition4.getBlockPos(), Blocks.AIR);
+                }
+            }
+        }
+    }
+
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         ItemStack stack = player.getStackInHand(hand);
         if (stack.getItem() == ModItems.GOLEM_SOUL) {
@@ -151,152 +187,29 @@ public class MixinCarvedPumkinBlock extends Block {
                     }
 
                 }
+                return ActionResult.CONSUME;
 
             } else if (laser.isPresent()) {
-                if (!player.abilities.creativeMode) {
-                    player.getStackInHand(hand).decrement(1);
-                }
-
-                int width = commonPattern.getWidth();
-                int height = commonPattern.getHeight();
-
-                for (int k = 0; k < width; ++k) {
-                    for (int l = 0; l < height; ++l) {
-                        CachedBlockPosition cachedBlockPosition3 = laser.get().translate(k, l, 0);
-                        world.setBlockState(cachedBlockPosition3.getBlockPos(), Blocks.AIR.getDefaultState(), 2);
-                        world.syncWorldEvent(2001, cachedBlockPosition3.getBlockPos(), Block.getRawIdFromState(cachedBlockPosition3.getBlockState()));
-                    }
-                }
                 Optional<LaserGolemEntity> golem = Optional.ofNullable(ModEntities.LASER_GOLEM.create(world));
-                if (golem.isPresent()) {
-                    golem.get().setPlayerCreated(true);
+                spawnGolem(world, player, hand, laser.get(), golem);
 
-                    BlockPos blockPos2 = laser.get().translate(1, 2, 0).getBlockPos();
-                    golem.get().refreshPositionAndAngles((double) blockPos2.getX() + 0.5D, (double) blockPos2.getY() + 0.05D, (double) blockPos2.getZ() + 0.5D, 0.0F, 0.0F);
-                    world.spawnEntity(golem.get());
-
-                    for (ServerPlayerEntity serverPlayer : world.getNonSpectatingEntities(ServerPlayerEntity.class, golem.get().getBoundingBox().expand(5.0D))) {
-                        Criteria.SUMMONED_ENTITY.trigger(serverPlayer, golem.get());
-                    }
-
-
-                    for (int m = 0; m < width; ++m) {
-                        for (int n = 0; n < height; ++n) {
-                            CachedBlockPosition cachedBlockPosition4 = laser.get().translate(m, n, 0);
-                            world.updateNeighbors(cachedBlockPosition4.getBlockPos(), Blocks.AIR);
-                        }
-                    }
-                }
+                return ActionResult.CONSUME;
 
             } else if (antiCreeper.isPresent()) {
-                if (!player.abilities.creativeMode) {
-                    player.getStackInHand(hand).decrement(1);
-                }
-
-                int width = commonPattern.getWidth();
-                int height = commonPattern.getHeight();
-
-                for (int k = 0; k < width; ++k) {
-                    for (int l = 0; l < height; ++l) {
-                        CachedBlockPosition cachedBlockPosition3 = antiCreeper.get().translate(k, l, 0);
-                        world.setBlockState(cachedBlockPosition3.getBlockPos(), Blocks.AIR.getDefaultState(), 2);
-                        world.syncWorldEvent(2001, cachedBlockPosition3.getBlockPos(), Block.getRawIdFromState(cachedBlockPosition3.getBlockState()));
-                    }
-                }
-
                 Optional<AntiCreeperGolemEntity> golem = Optional.ofNullable(ModEntities.ANTI_CREEPER_GOLEM.create(world));
-                if (golem.isPresent()) {
-                    golem.get().setPlayerCreated(true);
+                spawnGolem(world, player, hand, antiCreeper.get(), golem);
 
-                    BlockPos blockPos2 = antiCreeper.get().translate(1, 2, 0).getBlockPos();
-                    golem.get().refreshPositionAndAngles((double) blockPos2.getX() + 0.5D, (double) blockPos2.getY() + 0.05D, (double) blockPos2.getZ() + 0.5D, 0.0F, 0.0F);
-                    world.spawnEntity(golem.get());
-
-                    for (ServerPlayerEntity serverPlayer : world.getNonSpectatingEntities(ServerPlayerEntity.class, golem.get().getBoundingBox().expand(5.0D))) {
-                        Criteria.SUMMONED_ENTITY.trigger(serverPlayer, golem.get());
-                    }
-
-
-                    for (int m = 0; m < width; ++m) {
-                        for (int n = 0; n < height; ++n) {
-                            CachedBlockPosition cachedBlockPosition4 = antiCreeper.get().translate(m, n, 0);
-                            world.updateNeighbors(cachedBlockPosition4.getBlockPos(), Blocks.AIR);
-                        }
-                    }
-                }
+                return ActionResult.CONSUME;
 
             } else if (diamondLaser.isPresent()) {
-                if (!player.abilities.creativeMode) {
-                    player.getStackInHand(hand).decrement(1);
-                }
-
-                int width = commonPattern.getWidth();
-                int height = commonPattern.getHeight();
-
-                for (int k = 0; k < width; ++k) {
-                    for (int l = 0; l < height; ++l) {
-                        CachedBlockPosition cachedBlockPosition3 = diamondLaser.get().translate(k, l, 0);
-                        world.setBlockState(cachedBlockPosition3.getBlockPos(), Blocks.AIR.getDefaultState(), 2);
-                        world.syncWorldEvent(2001, cachedBlockPosition3.getBlockPos(), Block.getRawIdFromState(cachedBlockPosition3.getBlockState()));
-                    }
-                }
-
                 Optional<LaserGolemEntity> golem = Optional.ofNullable(ModEntities.DIAMOND_LASER_GOLEM.create(world));
-                if (golem.isPresent()) {
-                    golem.get().setPlayerCreated(true);
+                spawnGolem(world, player, hand, diamondLaser.get(), golem);
 
-                    BlockPos blockPos2 = diamondLaser.get().translate(1, 2, 0).getBlockPos();
-                    golem.get().refreshPositionAndAngles((double) blockPos2.getX() + 0.5D, (double) blockPos2.getY() + 0.05D, (double) blockPos2.getZ() + 0.5D, 0.0F, 0.0F);
-                    world.spawnEntity(golem.get());
+                return ActionResult.CONSUME;
 
-                    for (ServerPlayerEntity serverPlayer : world.getNonSpectatingEntities(ServerPlayerEntity.class, golem.get().getBoundingBox().expand(5.0D))) {
-                        Criteria.SUMMONED_ENTITY.trigger(serverPlayer, golem.get());
-                    }
-
-
-                    for (int m = 0; m < width; ++m) {
-                        for (int n = 0; n < height; ++n) {
-                            CachedBlockPosition cachedBlockPosition4 = diamondLaser.get().translate(m, n, 0);
-                            world.updateNeighbors(cachedBlockPosition4.getBlockPos(), Blocks.AIR);
-                        }
-                    }
-                }
             } else if (obama.isPresent()) {
-                if (!player.abilities.creativeMode) {
-                    player.getStackInHand(hand).decrement(1);
-                }
-
-                int width = commonPattern.getWidth();
-                int height = commonPattern.getHeight();
-
-                for (int k = 0; k < width; ++k) {
-                    for (int l = 0; l < height; ++l) {
-                        CachedBlockPosition cachedBlockPosition3 = obama.get().translate(k, l, 0);
-                        world.setBlockState(cachedBlockPosition3.getBlockPos(), Blocks.AIR.getDefaultState(), 2);
-                        world.syncWorldEvent(2001, cachedBlockPosition3.getBlockPos(), Block.getRawIdFromState(cachedBlockPosition3.getBlockState()));
-                    }
-                }
-
                 Optional<LaserGolemEntity> golem = Optional.ofNullable(ModEntities.OBAMA_PRISM_GOLEM.create(world));
-                if (golem.isPresent()) {
-                    golem.get().setPlayerCreated(true);
-
-                    BlockPos blockPos2 = obama.get().translate(1, 2, 0).getBlockPos();
-                    golem.get().refreshPositionAndAngles((double) blockPos2.getX() + 0.5D, (double) blockPos2.getY() + 0.05D, (double) blockPos2.getZ() + 0.5D, 0.0F, 0.0F);
-                    world.spawnEntity(golem.get());
-
-                    for (ServerPlayerEntity serverPlayer : world.getNonSpectatingEntities(ServerPlayerEntity.class, golem.get().getBoundingBox().expand(5.0D))) {
-                        Criteria.SUMMONED_ENTITY.trigger(serverPlayer, golem.get());
-                    }
-
-
-                    for (int m = 0; m < width; ++m) {
-                        for (int n = 0; n < height; ++n) {
-                            CachedBlockPosition cachedBlockPosition4 = obama.get().translate(m, n, 0);
-                            world.updateNeighbors(cachedBlockPosition4.getBlockPos(), Blocks.AIR);
-                        }
-                    }
-                }
+                spawnGolem(world, player, hand, obama.get(), golem);
 
                 return ActionResult.CONSUME;
             }
