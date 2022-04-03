526c95a9ee95cf015349dba31c76f0c06b9d928c
frqnny <45723631+frqnny@users.noreply.github.com>
2022-04-02 23:04:49 -0400

    Cleanup golem spawning

--- src/main/java/io/github/frqnny/golemsgalore/api/GolemSpawningUtil.java
+++ src/main/java/io/github/frqnny/golemsgalore/api/GolemSpawningUtil.java
@@ -1,7 +1,9 @@
 package io.github.frqnny.golemsgalore.api;
 
-import io.github.frqnny.golemsgalore.entity.ModGolemEntity;
+import io.github.frqnny.golemsgalore.entity.*;
 import io.github.frqnny.golemsgalore.init.ModBlocks;
+import io.github.frqnny.golemsgalore.init.ModEntities;
+import io.github.frqnny.golemsgalore.init.ModItems;
 import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -11,7 +13,9 @@ import net.minecraft.block.pattern.BlockPattern;
 import net.minecraft.block.pattern.BlockPatternBuilder;
 import net.minecraft.block.pattern.CachedBlockPosition;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.function.MaterialPredicate;
 import net.minecraft.util.math.BlockPos;
@@ -31,16 +35,17 @@ public class GolemSpawningUtil {
                     blockState.isOf(Blocks.HAY_BLOCK) ||
                     blockState.isOf(Blocks.AMETHYST_BLOCK)
     );
-    public static final BlockPattern commonPattern = getSingleBlockPattern(IS_VALID_BLOCK);
     public static final Predicate<BlockState> IRON_BLOCK_PREDICATE = getPredicateFromBlock(Blocks.IRON_BLOCK);
     public static final Predicate<BlockState> REDSTONE_BLOCK_PREDICATE = getPredicateFromBlock(Blocks.REDSTONE_BLOCK);
-    public static final BlockPattern laserPattern = getSpecialPattern(IRON_BLOCK_PREDICATE, REDSTONE_BLOCK_PREDICATE);
     public static final Predicate<BlockState> TNT_PREDICATE = getPredicateFromBlock(Blocks.TNT);
-    public static final BlockPattern antiCreeperPattern = getSpecialPattern(IRON_BLOCK_PREDICATE, TNT_PREDICATE);
     public static final Predicate<BlockState> DIAMOND_BLOCK_PREDICATE = getPredicateFromBlock(Blocks.DIAMOND_BLOCK);
-    public static final BlockPattern diamondLaserPattern = getSpecialPattern(DIAMOND_BLOCK_PREDICATE, REDSTONE_BLOCK_PREDICATE);
     public static final Predicate<BlockState> OBAMIUM_BLOCK_PREDICATE = getPredicateFromBlock(ModBlocks.OBAMIUM_BLOCK);
+    public static final BlockPattern commonPattern = getSingleBlockPattern(IS_VALID_BLOCK);
+    public static final BlockPattern laserPattern = getSpecialPattern(IRON_BLOCK_PREDICATE, REDSTONE_BLOCK_PREDICATE);
+    public static final BlockPattern antiCreeperPattern = getSpecialPattern(IRON_BLOCK_PREDICATE, TNT_PREDICATE);
+    public static final BlockPattern diamondLaserPattern = getSpecialPattern(DIAMOND_BLOCK_PREDICATE, REDSTONE_BLOCK_PREDICATE);
     public static final BlockPattern obamaPattern = getSingleBlockPattern(OBAMIUM_BLOCK_PREDICATE);
+    public static final BlockPattern ghastlyPattern = getSingleBlockPattern(IS_PUMPKIN_PREDICATE);
 
 
     public static Predicate<BlockState> getPredicateFromBlock(Block block) {
@@ -98,5 +103,94 @@ public class GolemSpawningUtil {
                 }
             }
         }
+    }
+
+
+    public static ActionResult golemHook(World world, BlockPos pos, PlayerEntity player, Hand hand) {
+        ItemStack stack = player.getStackInHand(hand);
+        if (stack.getItem() == ModItems.GOLEM_SOUL) {
+            BlockPattern.Result common = GolemSpawningUtil.commonPattern.searchAround(world, pos);
+            BlockPattern.Result laser = GolemSpawningUtil.laserPattern.searchAround(world, pos);
+            BlockPattern.Result antiCreeper = GolemSpawningUtil.antiCreeperPattern.searchAround(world, pos);
+            BlockPattern.Result diamondLaser = GolemSpawningUtil.diamondLaserPattern.searchAround(world, pos);
+            BlockPattern.Result obama = GolemSpawningUtil.obamaPattern.searchAround(world, pos);
+            BlockPattern.Result ghastly = GolemSpawningUtil.ghastlyPattern.searchAround(world, pos);
+
+            if (common != null) {
+                if (!player.isCreative()) {
+                    player.getStackInHand(hand).decrement(1);
+                }
+
+                Block block = null;
+                int width = GolemSpawningUtil.commonPattern.getWidth();
+                int height = GolemSpawningUtil.commonPattern.getHeight();
+                for (int k = 0; k < width; ++k) {
+                    for (int l = 0; l < height; ++l) {
+                        CachedBlockPosition cachedBlockPosition3 = common.translate(k, l, 0);
+                        if (GolemSpawningUtil.IS_VALID_BLOCK.test(cachedBlockPosition3.getBlockState())) {
+                            block = cachedBlockPosition3.getBlockState().getBlock();
+                        }
+                        world.setBlockState(cachedBlockPosition3.getBlockPos(), Blocks.AIR.getDefaultState(), 2);
+                        world.syncWorldEvent(2001, cachedBlockPosition3.getBlockPos(), Block.getRawIdFromState(cachedBlockPosition3.getBlockState()));
+                    }
+                }
+
+                BlockPos blockPos2 = common.translate(1, 2, 0).getBlockPos();
+                if (block != null) {
+                    Type type = Type.fromBlock(block);
+
+                    ModGolemEntity golem = ModEntities.typeMap.get(type).create(world);
+                    if (golem != null) {
+                        golem.setGolemType(type);
+                        golem.setPlayerCreated(true);
+                        golem.refreshPositionAndAngles((double) blockPos2.getX() + 0.5D, (double) blockPos2.getY() + 0.05D, (double) blockPos2.getZ() + 0.5D, 0.0F, 0.0F);
+                        world.spawnEntity(golem);
+
+                        for (ServerPlayerEntity serverPlayer : world.getNonSpectatingEntities(ServerPlayerEntity.class, golem.getBoundingBox().expand(5.0D))) {
+                            Criteria.SUMMONED_ENTITY.trigger(serverPlayer, golem);
+                        }
+
+                        for (int m = 0; m < width; ++m) {
+                            for (int n = 0; n < height; ++n) {
+                                CachedBlockPosition cachedBlockPosition4 = common.translate(m, n, 0);
+                                world.updateNeighbors(cachedBlockPosition4.getBlockPos(), Blocks.AIR);
+                            }
+                        }
+                    }
+
+                }
+                return ActionResult.CONSUME;
+
+            } else if (laser != null) {
+                LaserGolemEntity golem = ModEntities.LASER_GOLEM.create(world);
+                GolemSpawningUtil.spawnGolem(world, player, hand, laser, golem);
+
+                return ActionResult.CONSUME;
+
+            } else if (antiCreeper != null) {
+                AntiCreeperGolemEntity golem = ModEntities.ANTI_CREEPER_GOLEM.create(world);
+                GolemSpawningUtil.spawnGolem(world, player, hand, antiCreeper, golem);
+
+                return ActionResult.CONSUME;
+
+            } else if (diamondLaser != null) {
+                DiamondLaserGolemEntity golem = ModEntities.DIAMOND_LASER_GOLEM.create(world);
+                GolemSpawningUtil.spawnGolem(world, player, hand, diamondLaser, golem);
+
+                return ActionResult.CONSUME;
+
+            } else if (obama != null) {
+                ObamaPyramidGolemEntity golem = ModEntities.OBAMA_PRISM_GOLEM.create(world);
+                GolemSpawningUtil.spawnGolem(world, player, hand, obama, golem);
+
+                return ActionResult.CONSUME;
+            } else if (ghastly != null) {
+                GhastlyGolemEntity golem = ModEntities.GHASTLY_GOLEM.create(world);
+                GolemSpawningUtil.spawnGolem(world, player, hand, ghastly, golem);
+
+                return ActionResult.CONSUME;
+            }
+        }
+        return ActionResult.PASS;
     }
 }
--- src/main/java/io/github/frqnny/golemsgalore/mixin/MixinCarvedPumpkinBlock.java
+++ src/main/java/io/github/frqnny/golemsgalore/mixin/MixinCarvedPumpkinBlock.java
@@ -1,20 +1,10 @@
 package io.github.frqnny.golemsgalore.mixin;
 
 import io.github.frqnny.golemsgalore.api.GolemSpawningUtil;
-import io.github.frqnny.golemsgalore.api.Type;
-import io.github.frqnny.golemsgalore.entity.*;
-import io.github.frqnny.golemsgalore.init.ModEntities;
-import io.github.frqnny.golemsgalore.init.ModItems;
-import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.CarvedPumpkinBlock;
-import net.minecraft.block.pattern.BlockPattern;
-import net.minecraft.block.pattern.CachedBlockPosition;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -31,84 +21,6 @@ public abstract class MixinCarvedPumpkinBlock extends Block {
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-
-        ItemStack stack = player.getStackInHand(hand);
-        if (stack.getItem() == ModItems.GOLEM_SOUL) {
-            BlockPattern.Result common = GolemSpawningUtil.commonPattern.searchAround(world, pos);
-            BlockPattern.Result laser = GolemSpawningUtil.laserPattern.searchAround(world, pos);
-            BlockPattern.Result antiCreeper = GolemSpawningUtil.antiCreeperPattern.searchAround(world, pos);
-            BlockPattern.Result diamondLaser = GolemSpawningUtil.diamondLaserPattern.searchAround(world, pos);
-            BlockPattern.Result obama = GolemSpawningUtil.obamaPattern.searchAround(world, pos);
-            if (common != null) {
-                if (!player.isCreative()) {
-                    player.getStackInHand(hand).decrement(1);
-                }
-
-                Block block = null;
-                int width = GolemSpawningUtil.commonPattern.getWidth();
-                int height = GolemSpawningUtil.commonPattern.getHeight();
-                for (int k = 0; k < width; ++k) {
-                    for (int l = 0; l < height; ++l) {
-                        CachedBlockPosition cachedBlockPosition3 = common.translate(k, l, 0);
-                        if (GolemSpawningUtil.IS_VALID_BLOCK.test(cachedBlockPosition3.getBlockState())) {
-                            block = cachedBlockPosition3.getBlockState().getBlock();
-                        }
-                        world.setBlockState(cachedBlockPosition3.getBlockPos(), Blocks.AIR.getDefaultState(), 2);
-                        world.syncWorldEvent(2001, cachedBlockPosition3.getBlockPos(), Block.getRawIdFromState(cachedBlockPosition3.getBlockState()));
-                    }
-                }
-
-                BlockPos blockPos2 = common.translate(1, 2, 0).getBlockPos();
-                if (block != null) {
-                    Type type = Type.fromBlock(block);
-
-                    ModGolemEntity golem = ModEntities.typeMap.get(type).create(world);
-                    if (golem != null) {
-                        golem.setGolemType(type);
-                        golem.setPlayerCreated(true);
-                        golem.refreshPositionAndAngles((double) blockPos2.getX() + 0.5D, (double) blockPos2.getY() + 0.05D, (double) blockPos2.getZ() + 0.5D, 0.0F, 0.0F);
-                        world.spawnEntity(golem);
-
-                        for (ServerPlayerEntity serverPlayer : world.getNonSpectatingEntities(ServerPlayerEntity.class, golem.getBoundingBox().expand(5.0D))) {
-                            Criteria.SUMMONED_ENTITY.trigger(serverPlayer, golem);
-                        }
-
-                        for (int m = 0; m < width; ++m) {
-                            for (int n = 0; n < height; ++n) {
-                                CachedBlockPosition cachedBlockPosition4 = common.translate(m, n, 0);
-                                world.updateNeighbors(cachedBlockPosition4.getBlockPos(), Blocks.AIR);
-                            }
-                        }
-                    }
-
-                }
-                return ActionResult.CONSUME;
-
-            } else if (laser != null) {
-                LaserGolemEntity golem = ModEntities.LASER_GOLEM.create(world);
-                GolemSpawningUtil.spawnGolem(world, player, hand, laser, golem);
-
-                return ActionResult.CONSUME;
-
-            } else if (antiCreeper != null) {
-                AntiCreeperGolemEntity golem = ModEntities.ANTI_CREEPER_GOLEM.create(world);
-                GolemSpawningUtil.spawnGolem(world, player, hand, antiCreeper, golem);
-
-                return ActionResult.CONSUME;
-
-            } else if (diamondLaser != null) {
-                DiamondLaserGolemEntity golem = ModEntities.DIAMOND_LASER_GOLEM.create(world);
-                GolemSpawningUtil.spawnGolem(world, player, hand, diamondLaser, golem);
-
-                return ActionResult.CONSUME;
-
-            } else if (obama != null) {
-                ObamaPyramidGolemEntity golem = ModEntities.OBAMA_PRISM_GOLEM.create(world);
-                GolemSpawningUtil.spawnGolem(world, player, hand, obama, golem);
-
-                return ActionResult.CONSUME;
-            }
-        }
-        return ActionResult.PASS;
+        return GolemSpawningUtil.golemHook(world, pos, player, hand);
     }
 }
